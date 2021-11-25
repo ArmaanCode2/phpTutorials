4a8b35220bbc46cb0de15396128c6dc8ff3862f3
armaan <armaanvashisht67@gmail.com>
2021-11-25 20:47:06 +0530

    log in ke button pe click krne ke bad uss page pr chale jaye

--- My App/index.php
+++ My App/index.php
@@ -6,12 +6,12 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
-    <title>Hello, world!</title>
+    <title>Sign up</title>
   </head>
   <body>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Armaan</a>
+    <a class="navbar-brand" href="#">Sign up</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -19,7 +19,7 @@
       <div class="navbar-nav">
         <a class="nav-link active" aria-current="page" href="#">Home</a>
         <a class="nav-link" href="index.php">Sign up</a>
-        <a class="nav-link" href="#">Log in</a>
+        <a class="nav-link" href="log_in.php">Log in</a>
       </div>
     </div>
   </div>
@@ -64,7 +64,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
  if(!$conn){
      die("Sorry We failed to connect" . mysqli_connect_error());
  }else{
-     echo "connection was Success<br>";
+     echo "You Have Finally Signed up <br>";
  }
  
  $sql = "INSERT INTO `signin` (`name`, `email`, `password`) VALUES ('$name', '$email', '$password1')";
