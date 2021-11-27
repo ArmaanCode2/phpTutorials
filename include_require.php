45d4bdb11af7d45b37874e7e403e69122cba99ad
armaan <armaanvashisht67@gmail.com>
2021-11-27 23:42:18 +0530

    inlcude and require functions

---
+++ include_require.php
@@ -0,0 +1,5 @@
+<?php //difference between them
+include 'Function.php';//if file is not found it allows to go forward in code
+require 'Function.php';//if file not found it does not allow to forward in code
+
+?>
